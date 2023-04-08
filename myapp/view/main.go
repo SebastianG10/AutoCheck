14806d1d87c35cdde9baf9840f068f6d9b66ea0c
Jhon <jhon.rodriguez22329@ucaldas.edu.co>
2023-04-08 17:21:16 -0500

    inputs para entrada de la quintupla por interfaz grafica

--- myapp/view/main.go
+++ myapp/view/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"fmt"
-	"myapp/controller"
+	// "fmt"
+	// "myapp/controller"
 
 	// Imports para interfaz gráfica
 	"image/color"
@@ -17,37 +17,15 @@ import (
 	"fyne.io/fyne/v2/widget"
 )
 
+// paleta de colores
+var blue = color.NRGBA{R: 50, G: 119, B: 168, A: 0xff}
+
 // main es la función principal del programa.
 func main() {
 
 	interfaz()
 
-	control := &controller.ControlAutomatas{}
-
-	stateMap := control.ReadState("q0,q1")
-	symbols := control.ReadSymbols("0,1")
-	transitionsList := control.ReadTransitions("[q0,0,q1],[q0,1,q0],[q1,0,q1],[q1,1,q0]", stateMap)
-	initialState := control.ReadInitialState("q0")
-	finalStatesMap := control.ReadFinalStates("q1")
-
-	automata := control.CreateAutomata(transitionsList, initialState, finalStatesMap, stateMap, symbols)
-
-	// Prueba para ProcessInput
-	input := "1010"
-	automata.ProcessInput(input)
-	fmt.Printf("Después de procesar la entrada '%s', el estado actual es: %s\n", input, automata.GetCurrentState().GetName())
-
-	fmt.Println("Historial de estados actuales:")
-	for _, state := range automata.GetHistoryCurrentState() {
-		fmt.Printf("%s -> ", state.GetName())
-	}
-
-	// Prueba para IsAccepted
-	if automata.IsAccepted() {
-		fmt.Println("La entrada es aceptada por el autómata.")
-	} else {
-		fmt.Println("La entrada no es aceptada por el autómata.")
-	}
+	logicaAutomata()
 
 }
 
@@ -62,12 +40,95 @@ func interfaz() {
 	myApp.Run()
 }
 
-func logicContent() {
+func logicContent() *fyne.Container {
+	// control := &controller.ControlAutomatas{}
 
+	// stateMap := control.ReadState("q0,q1")
+	// symbols := control.ReadSymbols("0,1")
+	// transitionsList := control.ReadTransitions("[q0,0,q1],[q0,1,q0],[q1,0,q1],[q1,1,q0]", stateMap)
+	// initialState := control.ReadInitialState("q0")
+	// finalStatesMap := control.ReadFinalStates("q1")
+
+	// automata := control.CreateAutomata(transitionsList, initialState, finalStatesMap, stateMap, symbols)
+
+	// // Prueba para ProcessInput
+	// input := "1010"
+	// automata.ProcessInput(input)
+	// fmt.Printf("Después de procesar la entrada '%s', el estado actual es: %s\n", input, automata.GetCurrentState().GetName())
+
+	// fmt.Println("Historial de estados actuales:")
+	// for _, state := range automata.GetHistoryCurrentState() {
+	// 	fmt.Printf("%s -> ", state.GetName())
+	// }
+
+	// // Prueba para IsAccepted
+	// if automata.IsAccepted() {
+	// 	fmt.Println("La entrada es aceptada por el autómata.")
+	// } else {
+	// 	fmt.Println("La entrada no es aceptada por el autómata.")
+	// }
+
+	//------------------------------------------------------------------------------------------------------------------------------
+	instrucciones := canvas.NewText("Ingrese la quintupla del autómata siguiendo las indicaciones que se den", blue)
+
+	//ingreso de estados
+	statesInstruc := widget.NewLabel("Ingrese los estados de su autómata separandolos con comas\nEjemplo: q0,q1,q2...")
+	statesInput := widget.NewEntry()
+	statesCont := fyne.NewContainerWithLayout(layout.NewVBoxLayout(), statesInstruc, statesInput)
+
+	//ingreso de alfabeto
+	symbolsInstruc := widget.NewLabel("Ingrese los simbolos del alfabeto separandolos con comas\nEjemplo: 0,1...")
+	symbolsInput := widget.NewEntry()
+	symbolsCont := fyne.NewContainerWithLayout(layout.NewVBoxLayout(), symbolsInstruc, symbolsInput)
+
+	//ingreso de transiciones
+	transitionsInstruc := widget.NewLabel("Ingrese las transiciones del automata con el sigiente formato: [from,input,to]\nEjemplo: [q0,1,q0],[q1,0,q1],...")
+	transitionsInput := widget.NewEntry()
+	transitionsCont := fyne.NewContainerWithLayout(layout.NewVBoxLayout(), transitionsInstruc, transitionsInput)
+
+	//ingreso de alfabeto
+	initialInstruc := widget.NewLabel("Ingrese el estado inicial de su autómata\nEjemplo: q0")
+	initialInput := widget.NewEntry()
+	initialCont := fyne.NewContainerWithLayout(layout.NewVBoxLayout(), initialInstruc, initialInput)
+
+	//ingreso de transiciones
+	finalInstruc := widget.NewLabel("Ingrese las transiciones del automata con el sigiente formato: [from,input,to]\nEjemplo: [q0,1,q0],[q1,0,q1],...")
+	finalInput := widget.NewEntry()
+	finalCont := fyne.NewContainerWithLayout(layout.NewVBoxLayout(), finalInstruc, finalInput)
+
+	//botón para construir autómata
+	constAutomata := widget.NewButtonWithIcon("Construir Autómata", theme.ConfirmIcon(), func() {
+
+		// stateMap := control.ReadState(statesInput.SelectedText())
+		// symbols := control.ReadSymbols(symbolsInput.SelectedText())
+		// transitionsList := control.ReadTransitions(transitionsInput.SelectedText(), stateMap)
+		// initialState := control.ReadInitialState(initialInput.SelectedText())
+		// finalStatesMap := control.ReadFinalStates(finalInput.SelectedText())
+
+		// automata := control.CreateAutomata(transitionsList, initialState, finalStatesMap, stateMap, symbols)
+
+	})
+
+	// se puede intentar añadir fondo con un max y un rectangulo
+	cargarAutomatacont := fyne.NewContainerWithLayout(layout.NewVBoxLayout(),
+		instrucciones,
+		statesCont,
+		symbolsCont,
+		transitionsCont,
+		initialCont,
+		finalCont,
+		constAutomata,
+	)
+
+	content := fyne.NewContainerWithLayout(layout.NewHBoxLayout(),
+		cargarAutomatacont,
+		// renderizar automata en la parte derecha de la ventana
+		layout.NewSpacer(),
+	)
+	return content
 }
 
 func welcomeContent(app fyne.App) *fyne.Container {
-	blue := color.NRGBA{R: 50, G: 119, B: 168, A: 0xff}
 
 	infinite := widget.NewProgressBarInfinite()
 
@@ -85,7 +146,8 @@ func welcomeContent(app fyne.App) *fyne.Container {
 	// la función del parámetro del botón abre la ventana donde se desarrolla la lógica de la aplicaciónx
 	startButton := widget.NewButtonWithIcon("Empezar", theme.MediaPlayIcon(), func() {
 		var logicWindow = app.NewWindow("Autómatas")
-		// logicWindow.SetContent(logicContent())
+		logicWindow.Resize(fyne.NewSize(1000, 750))
+		logicWindow.SetContent(logicContent())
 		logicWindow.Show()
 	})
 
@@ -124,4 +186,33 @@ func welcomeContent(app fyne.App) *fyne.Container {
 		layout.NewSpacer(),
 	)
 	return content
+}
+
+func logicaAutomata() {
+	/* control := &controller.ControlAutomatas{}
+
+		stateMap := control.ReadState("q0,q1")
+		symbols := control.ReadSymbols("0,1")
+		transitionsList := control.ReadTransitions("[q0,0,q1],[q0,1,q0],[q1,0,q1],[q1,1,q0]", stateMap)
+		initialState := control.ReadInitialState("q0")
+		finalStatesMap := control.ReadFinalStates("q1")
+
+		automata := control.CreateAutomata(transitionsList, initialState, finalStatesMap, stateMap, symbols)
+
+	    // Prueba para ProcessInput
+	    input := "1010"
+	    automata.ProcessInput(input)
+	    fmt.Printf("Después de procesar la entrada '%s', el estado actual es: %s\n", input, automata.GetCurrentState().GetName())
+
+		fmt.Println("Historial de estados actuales:")
+		for _, state := range automata.GetHistoryCurrentState() {
+			fmt.Printf("%s -> ", state.GetName())
+		}
+
+		// Prueba para IsAccepted
+		if automata.IsAccepted() {
+			fmt.Println("La entrada es aceptada por el autómata.")
+		} else {
+			fmt.Println("La entrada no es aceptada por el autómata.")
+		} */
 }
